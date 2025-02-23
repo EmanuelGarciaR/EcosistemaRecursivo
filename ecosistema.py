99efe19b0e80e231ca381bb5107c7850e2490572
Emblask <salas.mangel96@gmail.com>
2025-02-22 20:31:34 -0500

    combined both codes, made it prettier

--- ecosistema.py
+++ ecosistema.py
@@ -1,94 +1,99 @@
 import random
 
+matrix_size = 10
+quantity_wolves = 2
+quantity_rabbits = 5
+quantity_plants = 10
+vida_inicial = 5
+empty = "."
+
 class Recursive:
+    def __init__(self, n):
+        self.n = n
+        self.cells = self.generate_recursive_matrix(n)
 
     @staticmethod
-    def generate_recursive_matrix(size:int, i:int =0, j:int=0, fila:list[int]=None, matrix: list[list[int]]= None):
-        if fila == None and matrix == None:
-            fila = []
+    def generate_recursive_matrix(n, row=0, matrix=None):
+        if matrix is None:
             matrix = []
-        if i == size: #Caso base cuando ya se tengan todas las filas en la matriz, se retorne la matriz
+        if row == n:
             return matrix
-        if j == size: #Caso base para evaluar si ya estamos en la útlima celda de una fila
-            matrix.append(fila)
-            return Recursive.generate_recursive_matrix(size, i + 1, 0, [], matrix)
-
-        random_organism = Recursive.generate_random_organism()
-        fila.append(random_organism) #Agregar número aleatorio a la fila actual
-
-        return Recursive.generate_recursive_matrix(size, i, j+1, fila, matrix)
+        
+        return Recursive.generate_recursive_matrix(n, row + 1, matrix + [[empty] * n])
     
-    @staticmethod
-    def generate_random_organism():
-        organism = random.choice(['P', 'R', 'W', "-"])
-        if organism == 'P':
-            return Plant()
-        elif organism == 'R':
-            return Rabbit()
-        elif organism == 'W':
-            return Wolf()
-        else: 
-            return Null()
-    
-    @staticmethod
-    def eat(self, matrix: list[list[int]], i: int, j: int):
-        if matrix[i][j] == 1:
-            self.initial_health += 1
-        return self.initial_health
+    def is_empty(self, x, y):
+        return self.cells[x][y] == empty
+
+    def put_organisms(self, organism, quantity):
+        if quantity == 0:
+            return
+        
+        x = random.randint(0, self.n - 1)
+        y = random.randint(0, self.n - 1)
+        
+        if self.is_empty(x, y):
+            self.cells[x][y] = organism
+            self.put_organisms(organism, quantity - 1)
+        else:
+            self.put_organisms(organism, quantity)
+        
+    def show_matrix(self, row=0, column=0):
+        if row >= self.n:
+            return
+        
+        if column >= self.n:
+            print()
+            self.show_matrix(row + 1, 0)
+            return
+        
+        celda = self.cells[row][column]
+        if isinstance(celda, Organism):
+            print(celda.symbol, end=' ')
+        else:
+            print(celda, end=' ')
+        
+        self.show_matrix(row, column + 1)
 
 
 class Organism:
-    def __init__(self, initial_health: int):
+    def __init__(self, initial_health: int, symbol):
         self.initial_health: int = initial_health
+        self.symbol = symbol
 
     def is_life(self):
         return self.initial_health > 0
     
     def ageing(self):
         self.initial_health -= 1
-        return self.initial_health#Comprobar si se necesita esta linea
+        return self.initial_health
+
 
 class Wolf(Organism):
     def __init__(self):
-        super().__init__(initial_health = 5)
+        super().__init__(initial_health=5, symbol="W")
     
     def __repr__(self):
         return "W"
-    
-    #def movement_matrix(self):
-        
+
 
 class Plant(Organism):
     def __init__(self):
-        super().__init__(initial_health = 1)
+        super().__init__(initial_health=1, symbol="P")
     
     def __repr__(self):
         return "P"
 
+
 class Rabbit(Organism):
     def __init__(self):
-        super().__init__(initial_health = 3)
+        super().__init__(initial_health=3, symbol="R")
         
     def __repr__(self):
         return "R"
 
-class Null(Organism):
-    def __init__(self):
-        super().__init__(initial_health = 0)
-    
-    def __repr__(self):
-        return "-"
-class Matrix:
-    def __init__(self, size: int):
-        self.size = size
-        self.matriz = Recursive.generate_recursive_matrix(size)
-
-    def show_format_matrix(self, i: int= 0):
-        if i == self.size:
-            return
-        print(self.matriz[i])
-        return self.show_format_matrix(i+1)
-
-matriz = Matrix(5)
-matriz.show_format_matrix()
+matrix = Recursive(matrix_size)
+matrix.put_organisms(Wolf(), quantity_wolves)
+matrix.put_organisms(Rabbit(), quantity_rabbits)
+matrix.put_organisms(Plant(), quantity_plants)
+matrix.show_matrix()
 
